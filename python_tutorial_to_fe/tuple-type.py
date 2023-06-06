052cef5f06a41cb620f21dbbc6a05b4593663336
Aaron0502 <13126675855@gmail.com>
2023-06-06 21:58:38 +0800

    feat: 添加tuple类型

---
+++ python_tutorial_to_fe/tuple-type.py
@@ -0,0 +1,10 @@
+my_tuple = (1, 2, 3)
+print(my_tuple)  # (1, 2, 3)
+my_tuple = 1, 2, 3, 4
+print(my_tuple)  # (1, 2, 3, 4)
+
+tup1 = (50)
+print(type(tup1))  # <class 'int'>
+
+tup1 = (50,)
+print(type(tup1))  # <class 'tuple'>
